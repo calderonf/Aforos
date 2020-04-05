fd7839797a3a6a826c46b29ae4d6521b5c2689f3
Francisco Calderon <calderonf@gmail.com>
2020-04-05 23:12:55 +0000

    aforos listos- faltan reportes.

---
+++ Peticion_4/sync.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+rsync -vr "/monitoreo/gdrive/CM20191866/3. VIDEO/03032020/" /monitoreo/Aforos/Peticion_4/03032020/
+rsync -vr "/monitoreo/gdrive/CM20191866/3. VIDEO/04032020/" /monitoreo/Aforos/Peticion_4/04032020/
+rsync -vr "/monitoreo/gdrive/CM20191866/3. VIDEO/05032020/" /monitoreo/Aforos/Peticion_4/05032020/
+
+#05032020_KR7AXCL183SD114_0000/KR7XCL183SD114_20200305072500_0000.mp4
